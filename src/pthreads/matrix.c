bff4a145f218ea316739dc3d48a3963f80ed0c98
balhar <balhar.jakub@gmail.com>
2014-01-14 12:31:12 +0100

    Finished paralelization with variable amount of threads for matrix.

--- src/pthreads/matrix.c
+++ src/pthreads/matrix.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "../matrix.h"
 #include "../types.h"
 #include "../memory.h"
 #include "../random.h"
+#include "../codes.h"
+#include "../print.hpp"
 
 void countOneCell(floattype **matrix, floattype **gramMatrix, size_t row, size_t cell, size_t columns);
+
+typedef struct {
+    floattype **matrix;
+    floattype **gramMatrix;
+    
+    size_t row;
+    size_t cell;
+    size_t columns;
+    size_t cells;
+} CountCellParameters;
+void *countOneCellHandler(void *arg);
+int cellsPerThread = 4;
 
 /*
  * It gets pointer to matrix and fills the matrix with random data.
@@ -44,14 +59,44 @@ floattype **GenerateMatrix(floattype **matrix,
         return NULL;
     }
     floattype **gramMatrix = AllocMatrix(rows, (rows+1));
-
+    int amountOfTasks = (rows * rows);
+    int actualTask = 0, result = CODE_OK;
+    CountCellParameters *params[amountOfTasks];
+    pthread_t thread[amountOfTasks];
+    
     for(size_t row = 0; row < rows; row++) {
         for(size_t cell=0; cell < rows; cell++){
+            params[actualTask] = (CountCellParameters*) malloc (sizeof(CountCellParameters));
+            params[actualTask]->cell = cell;
+            params[actualTask]->columns = columns;
+            params[actualTask]->row = row;
+            params[actualTask]->matrix = matrix;
+            params[actualTask]->gramMatrix = gramMatrix;
+            if(cell + cellsPerThread >= rows) {
+                params[actualTask]->cells = rows - cell;
+            } else {
+                params[actualTask]->cells = cellsPerThread;
+            }
+            
+            
             //Go through all the positions in the final matrix.
-            countOneCell(matrix, gramMatrix, row, cell, columns);
+            result = pthread_create(&thread[actualTask], NULL, countOneCellHandler, params[actualTask]);
+            if(result != CODE_OK) {
+                return NULL;
+            }
+            //countOneCell(matrix, gramMatrix, row, cell, columns);
+            actualTask++;
         }
         gramMatrix[row][rows] = results[row];
     }
+    for(int i = 0; i < amountOfTasks; i++) {
+        result = pthread_join(thread[i], NULL);
+        if(result != CODE_OK) {
+            return NULL;
+        }
+        free(params[i]);
+    }
+            
     return gramMatrix;
 }
 
@@ -61,4 +106,13 @@ void countOneCell(floattype **matrix, floattype **gramMatrix, size_t row, size_t
         sum += (matrix[row][positionInVector] * matrix[cell][positionInVector]);
     }
     gramMatrix[row][cell] = sum;
+}
+
+void *countOneCellHandler(void *args) {
+    CountCellParameters *params = (CountCellParameters*)args;
+    for(size_t i = 0; i < params->cells; i++) {
+        countOneCell(params->matrix, params->gramMatrix, params->row, params->cell + i,
+            params->columns);
+    }
+    return NULL;
 }
